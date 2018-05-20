87aafb186beb88474d7df7fa57e788dc617d3c5d
Unknown <fwildanf@gmail.com>
2018-05-20 20:57:11 +0700

    Profile UI

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -5,7 +5,48 @@
 	<title>Moe Online Judge</title>
 	@yield('head')
 </head>
-<body style="background-color: #000000">
+<body>
+	@include('template.navigator')
+	<div class="ui container" style="min-height: 85vh">
+		@yield('content')
+	</div>
+	@include('template.footer')
+    @yield('script')
+    <script>
+        $(window).scroll(
+            function() {
+                if(isElementInViewport($("#footer")[0])){
+                    console.log('asdasd')
+                    $("#footer-navbar").hide();
+                }
+                else{
+                    $("#footer-navbar").show();
+                }
+            }
+        );
 
+        function isElementInViewport(el) {
+            var rect = el.getBoundingClientRect();
+
+            var top = el.offsetTop;
+            var left = el.offsetLeft;
+            var width = el.offsetWidth;
+            var height = el.offsetHeight;
+
+            while(el.offsetParent) {
+                el = el.offsetParent;
+                top += el.offsetTop;
+                left += el.offsetLeft;
+            }
+            console.log(top)
+            console.log(window.pageYOffset + window.innerHeight)
+            return (
+                top < (window.pageYOffset + window.innerHeight)-55 &&
+                left < (window.pageXOffset + window.innerWidth)-55 &&
+                (top + height) > window.pageYOffset &&
+                (left + width) > window.pageXOffset
+            );
+        }
+    </script>
 </body>
 </html>
