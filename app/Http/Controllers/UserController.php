2eb4065d9a9ecd64f407267e3a41feb4524c9ed0
Unknown <fwildanf@gmail.com>
2018-05-20 11:24:53 +0700

    User Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function show($id){
+    	$user = User::where('id',$id)->first();
+    	return view('user.show', compact('user'));
+    }
 }
