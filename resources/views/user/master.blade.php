bb3abef8b3e42ff74ea731321c33b52b8bd756bc
Unknown <fwildanf@gmail.com>
2018-05-20 12:37:56 +0700

    fix

---
+++ resources/views/user/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	@include('template.head')
+	<title>Moe Online Judge</title>
+	@yield('head')
+</head>
+<body style="background-color: #000000">
+
+</body>
+</html>
