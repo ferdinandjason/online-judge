685140196a1bc214657c670d2482aab8c0f424dc
William AD <w.albertusd@gmail.com>
2018-06-25 21:33:17 +0700

    Cleanup Model

--- app/Comment.php
+++ app/Comment.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
     protected $fillable = ['problem_id','user_id','comment'];
 
     public function user(){
--- app/User.php
+++ app/User.php
@@ -2,27 +2,17 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'username','real_name', 'email', 'password','school','github','major'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
